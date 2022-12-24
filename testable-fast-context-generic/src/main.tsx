55c7c6cde20f667f92f4124eee12f229cb54b54c
Justin <calleja.justin@gmail.com>
2022-12-24 12:50:22 +0100

    add todos app placeholder

--- testable-fast-context-generic/src/main.tsx
+++ testable-fast-context-generic/src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import NamesApp from './App';
+import TodosApp from './todos/App';
+import './index.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <BrowserRouter>
+      <nav>
+        <Link style={{ marginRight: '8px' }} to="/">
+          Names
+        </Link>
+        <Link to="/todos">Todos</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<NamesApp />} />
+        <Route path="/todos" element={<TodosApp />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
---
+++ testable-fast-context-generic/src/todos/App.tsx
@@ -0,0 +1,3 @@
+export default function App() {
+  return <div className="container">hello from todos app</div>;
+}
